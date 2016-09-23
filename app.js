63804e8114ae46ccfa87752452ca654fc30b947e
Louis Vela <louis.vela@gmail.com>
2016-09-23 17:45:51 -0500

    Add co to package.json. Cleanup old code.

--- app.js
+++ app.js
@@ -6,30 +6,6 @@ var r = require('request');
 var request = promisify(r, {multiArgs: true});
 var co = require('co');
 
-
-//request('http://geekdom.com/events/feed', function (error, response, body) {
-  //if (!error && response.statusCode == 200) {
-    ////console.log(body);
-    //var json = parser.toJson(body);
-    //console.log("entries: " + json.responseData.entries);
-    ////console.log("json = \n" + parser.toJson(body));
-  //}
-//});
-
-//request('http://geekdom.com/events/feed').then((res) => {
-  //var response = res[0];
-  //var body = res[1];
-
-  //if (response.statusCode == 200) {
-    ////console.log(body);
-    //var json = parser.toJson(body);
-    //console.log("entries: " + json.responseData.entries);
-    ////console.log("json = \n" + parser.toJson(body));
-  //}
-//}).catch((error) => {
-  //console.log(error);
-//});
-
 co(function *() {
   var res = yield request('http://geekdom.com/events/feed');
   //[response, body] = res;// destructuring isn't supported in this node version :(
